4078c07f6f8901b3bfacb3cbf4344ea958eec516
Dhrishika M <dhrishika@Dhrishikas-MacBook-Air.local>
2018-03-01 00:07:51 +0300

    trying to deploy

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,10 @@
+export const FIREBASE_CONFIG = {
+
+        apiKey: "AIzaSyCNbEVLdAkqu0rlbXlAZBeNsyEzInvfEPE",
+        authDomain: "ionicshoppinglist-f377c.firebaseapp.com",
+        databaseURL: "https://ionicshoppinglist-f377c.firebaseio.com",
+        projectId: "ionicshoppinglist-f377c",
+        storageBucket: "",
+        messagingSenderId: "406564574689"
+
+};
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
+import { ShoppingListService } from '../../services/shopping-list/shopping-list.service';
+import { Observable } from 'rxjs/Observable';
+import { Item } from '../../models/item/item.model';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  shoppingList$: Observable<Item[]>
+  constructor(public navCtrl: NavController, private shopping: ShoppingListService) {
+    this.shoppingList$ = this.shopping
+      .getShoppingList() // Returns a DB List
+      .snapshotChanges() // Gives us access to Key and Value Pairs
+      .map(
+        changes => {
+          return changes.map(c => ({
+            key: c.payload.key,
+            ...c.payload.val()
+          }));
+        });
   }
+/*   {
+    key: 'value-here',
+    name: 'iPad Pro',
+
+  } */
 
 }
